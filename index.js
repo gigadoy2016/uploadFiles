0c79cfef1e4982d048aa91cd0714d61dee674d3c
gigadoy2016 <rangsan_m@hotmail.com>
2024-01-30 09:50:38 +0700

    20240130 9:50

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
 var express = require("express");                           //import express ด้วยการใช้ require
 var app = express();                                        //set express ไว้เป็นตัวแปร app
-const multer  = require('multer')
-const upload = multer({ dest: 'uploads/' })
+const multer  = require('multer');
 const PORT = 3001;                                          //set ตัวแปร port เท่ากับ 3001
+
+
+const storage = multer.diskStorage({
+    destination: function (req, file, callback) {
+      callback(null, './docFiles') // folder ที่เราต้องการเก็บไฟล์
+    },
+    filename: function (req, file, callback) {
+      callback(null, file.originalname) //ให้ใช้ชื่อไฟล์ original เป็นชื่อหลังอัพโหลด
+    },
+})
+const upload = multer({ storage });
+
 
 //ใช้ get เพื่อเรียกไฟล์ index.html
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
-})
+});
 
 //ใช้ post เพื่อรองรับการ upload
 app.post('/upload', upload.single('photo'), (req, res) => {
     res.send(req.file)
-})
+});
 
-// ใช้ listen เพื่อระบุว่า website จะทำงานที่ port อะไร เราใช้ให้เรียกตัวแปร port
 app.listen(PORT, () => {
     console.log(`Server running at <http://localhost>:${PORT}/`);
-})
+});
+
